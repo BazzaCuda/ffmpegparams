3d4f50e4829178afeb7fd3a2e7388bc35634a431
BazzaCuda <bazzacuda@gmx.com>
2023-09-27 16:10:14 +0100

    Initial commit

---
+++ Unit1.dfm
@@ -0,0 +1,194 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'ffmpeg params'
+  ClientHeight = 423
+  ClientWidth = 1115
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  DesignSize = (
+    1115
+    423)
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 15
+    Top = 8
+    Width = 50
+    Height = 13
+    Caption = 'Input Files'
+  end
+  object Label2: TLabel
+    Left = 15
+    Top = 213
+    Width = 34
+    Height = 13
+    Caption = 'Output'
+  end
+  object Memo1: TMemo
+    Left = 8
+    Top = 27
+    Width = 845
+    Height = 174
+    ScrollBars = ssBoth
+    TabOrder = 0
+    WordWrap = False
+  end
+  object Memo2: TMemo
+    Left = 8
+    Top = 229
+    Width = 847
+    Height = 186
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    ScrollBars = ssBoth
+    TabOrder = 1
+    WordWrap = False
+  end
+  object Panel1: TPanel
+    Left = 861
+    Top = 0
+    Width = 254
+    Height = 423
+    Align = alRight
+    TabOrder = 2
+    object Label6: TLabel
+      Left = 23
+      Top = 48
+      Width = 45
+      Height = 13
+      Caption = 'Log Level'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -11
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+    object Label3: TLabel
+      Left = 22
+      Top = 99
+      Width = 70
+      Height = 13
+      Caption = 'Input switches'
+    end
+    object Label4: TLabel
+      Left = 21
+      Top = 155
+      Width = 79
+      Height = 13
+      Caption = 'Output Switches'
+    end
+    object Label5: TLabel
+      Left = 21
+      Top = 213
+      Width = 103
+      Height = 13
+      Caption = 'Output File Extension'
+    end
+    object btnClear: TButton
+      Left = 16
+      Top = 12
+      Width = 97
+      Height = 25
+      Caption = 'Clear Both'
+      TabOrder = 0
+      OnClick = btnClearClick
+    end
+    object edtLogLevel: TEdit
+      Left = 16
+      Top = 62
+      Width = 225
+      Height = 24
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 1
+      Text = '-loglevel error'
+    end
+    object edtInputSwitches: TEdit
+      Left = 16
+      Top = 112
+      Width = 225
+      Height = 24
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 2
+      Text = '-i'
+    end
+    object cbOutputSwitches: TComboBox
+      Left = 16
+      Top = 170
+      Width = 225
+      Height = 24
+      DropDownCount = 99
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 3
+      Text = '-c copy'
+    end
+    object edtFileExt: TEdit
+      Left = 16
+      Top = 228
+      Width = 57
+      Height = 24
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 4
+      Text = '.mp4'
+    end
+    object chbOverwrite: TCheckBox
+      Left = 12
+      Top = 299
+      Width = 141
+      Height = 17
+      Caption = 'Overwrite convert.bat'
+      Checked = True
+      State = cbChecked
+      TabOrder = 5
+    end
+    object btnClearOutput: TButton
+      Left = 14
+      Top = 262
+      Width = 75
+      Height = 25
+      Caption = 'Clear Output'
+      TabOrder = 6
+      OnClick = btnClearOutputClick
+    end
+    object btnGo: TButton
+      Left = 14
+      Top = 326
+      Width = 137
+      Height = 75
+      Caption = 'Go'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -32
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 7
+      OnClick = btnGoClick
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,129 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Mask;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    Label1: TLabel;
+    Memo2: TMemo;
+    Label2: TLabel;
+    Panel1: TPanel;
+    btnClear: TButton;
+    Label6: TLabel;
+    edtLogLevel: TEdit;
+    Label3: TLabel;
+    edtInputSwitches: TEdit;
+    Label4: TLabel;
+    cbOutputSwitches: TComboBox;
+    Label5: TLabel;
+    edtFileExt: TEdit;
+    chbOverwrite: TCheckBox;
+    btnClearOutput: TButton;
+    btnGo: TButton;
+    procedure btnGoClick(Sender: TObject);
+    procedure btnClearClick(Sender: TObject);
+    procedure btnClearOutputClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    function saveToFile(aPath: string): boolean;
+    function formatFileSize(const aSize: int64): string;
+    function getFileSize(const aFilePath: string): int64;
+    function longestLine: integer;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btnClearClick(Sender: TObject);
+begin
+  memo1.clear;
+  memo2.clear;
+end;
+
+procedure TForm1.btnClearOutputClick(Sender: TObject);
+begin
+  memo2.clear;
+end;
+
+function TForm1.saveToFile(aPath: string): boolean;
+const
+  convert = 'zzz_convert';
+begin
+  case chbOverwrite.checked of TRUE: memo2.lines.saveToFile(aPath + convert + '.bat'); end;
+  case chbOverwrite.checked of TRUE: EXIT; end;
+
+  var i: integer := 0;
+  var FN: string;
+  FN := convert + '.bat';
+  while fileExists(aPath + FN) do begin
+    inc(i);
+    FN := convert + intToStr(i) + '.bat';
+  end;
+  memo2.lines.saveToFile(aPath + FN);
+end;
+
+function TForm1.formatFileSize(const aSize: int64): string;
+begin
+ case aSize >= 1052266987 of  TRUE: try result := format('%.3f GB', [aSize / 1024 / 1024 / 1024]); except end;  // >= 0.98 of 1GB
+                             FALSE: case aSize < 1024 * 1024 of  TRUE: try result := format('%d KB', [trunc(aSize / 1024)]); except end;
+                                                                FALSE: try result := format('%.2f MB', [aSize / 1024 / 1024]); except end;end;end;
+end;
+
+function TForm1.getFileSize(const aFilePath: string): int64;
+var
+  vHandle:  THandle;
+  vRec:     TWin32FindData;
+begin
+  vHandle := findFirstFile(PChar(aFilePath), vRec);
+  case vHandle <> INVALID_HANDLE_VALUE of TRUE: begin
+                                                  winAPI.windows.findClose(vHandle);
+                                                  case (vRec.dwFileAttributes and FILE_ATTRIBUTE_DIRECTORY) = 0 of TRUE:
+                                                    result := (int64(vRec.nFileSizeHigh) shl 32) + vRec.nFileSizeLow; end;end;end;
+end;
+
+function TForm1.longestLine: integer;
+begin
+  result := 90;
+  for var i := 0 to memo2.lines.count - 1 do
+    case (pos(':::', memo2.lines[i]) > 0) AND (length(memo2.lines[i]) > result) of TRUE: result := length(memo2.lines[i]); end;
+end;
+
+procedure TForm1.btnGoClick(Sender: TObject);
+var
+  FN: string;
+begin
+  memo2.clear;
+  memo2.lines.loadFromFile(changeFileExt(paramStr(0), '.bat'));
+  for var i := memo1.lines.count - 1 downto 0 do begin
+    FN := changeFileExt(memo1.lines[i], '');
+    FN := FN + ' [c]' + edtFileExt.text;
+    var ff := format('@ffmpeg %s %s "%s" %s "%s"', [edtLogLevel.text, edtInputSwitches.text, memo1.lines[i], cbOutputSwitches.text, FN]);
+    memo2.lines.insert(6, '@echo.');
+    memo2.lines.insert(6, '@echo ::: ' + extractFileName(memo1.lines[i]) + ': ' + formatFileSize(getFileSize(memo1.lines[i])));
+    memo2.lines.insert(7, ff);
+  end;
+  memo2.lines.insert(3, 'mode con lines=30 cols=' + intToStr(longestLine + 6));
+
+  var FP := extractFilePath(memo1.lines[0]);
+  saveToFile(FP);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  case fileExists(changeFileExt(paramStr(0), '.ini')) of TRUE: cbOutputSwitches.Items.loadFromFile(changeFileExt(paramStr(0), '.ini')); end;
+  cbOutputSwitches.itemIndex := cbOutputSwitches.items.count - 1;
+end;
+
+end.
